3bc4f5151054c72caf12bd2109796f4198554bf7
Neon-20 <rajveer23pr@gmail.com>
2024-12-17 19:33:22 +0530

    remove number

--- app/(main)/support/page.tsx
+++ app/(main)/support/page.tsx
@@ -6,12 +6,12 @@ export default function SupportPage() {
       <main className="flex-1 bg-[#ffffff07] text-white p-8 m-8 rounded-2xl mr-5 min-h-[70vh]">
         <h2 className="text-2xl font-bold mb-8">Contact Us</h2>
         <div className="grid grid-cols-2 gap-8">
-          <div className="bg-[#ffffff08] hover:bg-[#ffffff0e] duration-200 border border-white/5 rounded-2xl shadow-lg shadow-black/10 p-6 flex flex-col justify-between">
-            <p className="text-xl font-bold ">Phone: +91 7781041905</p>
+           <div className="bg-[#ffffff08] hover:bg-[#ffffff0e] duration-200 border border-white/5 rounded-2xl shadow-lg shadow-black/10 p-6 flex flex-col justify-between">
+             <p className="text-xl font-bold">Email: davendra.patel@alterdomus.com</p>
           </div>
           <div className="bg-[#ffffff08] hover:bg-[#ffffff0e] duration-200 border border-white/5 rounded-2xl shadow-lg shadow-black/10 p-6 flex flex-col justify-between">
             <Link href="mailto:pranav.rajveer@alterdomus.com">
-              <p className="text-xl font-bold">Email: pranav.rajveer@alterdomus.com</p>
+              <p className="text-xl font-bold">Alternate Email: pranav.rajveer@alterdomus.com</p>
             </Link>
           </div>
         </div>
