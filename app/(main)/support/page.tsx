9f6ef7cb67b68910f3d259713e1258a8cb87cfa0
ShubhamMewara <shubhammewara12@gmail.com>
2025-02-05 21:21:25 +0530

    fixed err

--- app/(main)/support/page.tsx
+++ app/(main)/support/page.tsx
@@ -4,7 +4,7 @@ export default function SupportPage() {
   return (
     <div className="flex flex-col bg-[#0e0e0e] min-h-[95vh]">
       <main className="flex-1 bg-[#ffffff07] text-white p-8 m-8 rounded-2xl mr-5 min-h-[70vh]">
-        <h2 className="text-2xl font-bold mb-8">Contact Us</h2>
+        <h2 className="text-2xl font-bold mb-8"> Contact Us </h2>
         <div className="grid grid-cols-2 gap-8">
           <div className="bg-[#ffffff08] hover:bg-[#ffffff0e] duration-200 border border-white/5 rounded-2xl shadow-lg shadow-black/10 p-6 flex flex-col justify-between">
             <Link href="mailto:pranav.rajveer@alterdomus.com">
